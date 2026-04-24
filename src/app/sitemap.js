6d34dd8da0f659d43f6c8aac7f1f68e4ce7ad599
chen-feng520 <chen-feng520@163.com>
2026-04-24 14:50:13 +0800

    sitemap added

---
+++ src/app/sitemap.js
@@ -0,0 +1,11 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://guangzhouprivatedriver.top', // 这里换成你的真实域名
+      lastModified: new Date(),
+      changeFrequency: 'weekly', // 告诉谷歌这个网页每周可能会有更新
+      priority: 1.0, // 权重最高
+    },
+    
+  ]
+}
